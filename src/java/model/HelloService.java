c0944d3aaea3a0c7bfb888b8384db16eda6639bf
joshuabrown <joshuabrown@10.30.21.122>
2016-01-25 18:52:53 -0600

    Switched to mvc branch

---
+++ src/java/model/HelloService.java
@@ -0,0 +1,14 @@
+
+package model;
+
+/**
+ *
+ * @author joshuabrown
+ */
+public class HelloService {
+    
+    public String sayHello(String name){
+        return "Hello " + name + " , isnt MVC great!";
+    }
+    
+}
